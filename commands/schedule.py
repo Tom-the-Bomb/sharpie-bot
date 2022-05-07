30ce07696efb623fb202f1b9c49c589524a03a66
JacobK <techsupport@kreindler.ca>
2022-05-07 00:11:56 -0400

    fix: update `.gitignore`

--- commands/schedule.py
+++ commands/schedule.py
@@ -10,15 +10,15 @@ class schedule(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@cog_ext.cog_slash(name="schedule", description="Sends robotics schedule", guild_ids=guild_ids, options=[])
-async def _schedule(ctx):
-    embed = discord.Embed(title="Robotics Club Schedule")
-    embed.add_field(name='Monday', value='No Robotics', inline=False)
-    embed.add_field(name='Tuesday', value='3:00 - When everyone leaves', inline=False)
-    embed.add_field(name='Wednesday', value='3:00 - When everyone leaves', inline=False)
-    embed.add_field(name='Thursday', value='3:00 - When everyone leaves', inline=False)
-    embed.add_field(name='Friday', value='3:00 - When everyone leaves earlier', inline=False)
-    await ctx.send(embed=embed)
+    @cog_ext.cog_slash(name="schedule", description="Sends robotics schedule", guild_ids=guild_ids, options=[])
+    async def _schedule(self, ctx):
+        embed = discord.Embed(title="Robotics Club Schedule")
+        embed.add_field(name='Monday', value='No Robotics', inline=False)
+        embed.add_field(name='Tuesday', value='3:00 - When everyone leaves', inline=False)
+        embed.add_field(name='Wednesday', value='3:00 - When everyone leaves', inline=False)
+        embed.add_field(name='Thursday', value='3:00 - When everyone leaves', inline=False)
+        embed.add_field(name='Friday', value='3:00 - When everyone leaves earlier', inline=False)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
