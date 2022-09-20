ee5cae38dc6bd9ee926b70c92c28088332cc81c3
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-09-20 09:15:57 -0400

    refactor annoy.py

--- commands/annoy.py
+++ commands/annoy.py
@@ -1,31 +1,41 @@
 import interactions
-guild_ids = [918591198799749240]
 
-class annoy(interactions.Extension):
-    def __init__(self, bot):
-        self.bot: interactions.Client = bot
+GUILD_IDS = [918591198799749240]
 
-    @interactions.extension_command(name="annoy", description="Annoy someone", options=[
-        interactions.Option (
-            name="user",
-            description="The user to annoy",
-            type=interactions.OptionType.USER,
-            required=True
-        ),
-        interactions.Option (
-            name="amount",
-            description="The amount of times to annoy the user",
-            type=interactions.OptionType.INTEGER,
-            required=True
-        )
-    ])
-    async def annoy(self, ctx, user, amount):
+class Annoy(interactions.Extension):
+    def __init__(self, bot: interactions.Client) -> None:
+        self.bot = bot
+
+    @interactions.extension_command(
+        name="annoy",
+        description="Annoy someone",
+        options=[
+            interactions.Option(
+                name="user",
+                description="The user to annoy",
+                type=interactions.OptionType.USER,
+                required=True
+            ),
+            interactions.Option(
+                name="amount",
+                description="The amount of times to annoy the user",
+                type=interactions.OptionType.INTEGER,
+                required=True
+            ),
+        ]
+    )
+    async def annoy(
+        self,
+        ctx: interactions.CommandContext,
+        user: interactions.User,
+        amount: int,
+    ) -> None:
         if amount > 10:
             await ctx.send("That's just too far")
             return
         else:
-            for i in range(int(amount)):
-                await ctx.send(f"{user.mention}")
+            for i in range(amount):
+                await ctx.send(user.mention)
 
-def setup(bot):
-    annoy(bot)
+def setup(bot: interactions.Client) -> None:
+    Annoy(bot)
