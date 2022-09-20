45ddba600bd861d174a5b16af6729ecb9085816f
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-09-20 09:20:31 -0400

    refactor help.py

--- commands/help.py
+++ commands/help.py
@@ -1,20 +1,36 @@
 import interactions
 
-guild_ids = [918591198799749240]
+GUILD_IDS = [918591198799749240]
 
 
-class help(interactions.Extension):
-    def __init__(self, bot):
-        self.bot: interactions.Client = bot
+class Help(interactions.Extension):
+    def __init__(self, bot: interactions.Client) -> None:
+        self.bot = bot
 
     @interactions.extension_command(name="help", description="Lists available commands")
-    async def _help(self, ctx):
+    async def help(self, ctx: interactions.CommandContext) -> None:
         embed = interactions.Embed(title="Help")
-        embed.add_field(name="Annoy Someone", value="`/annoy` \n Pings a given user a specified number of times. Only works for <@&918640986618486794>'s <@&918638863516328056>'s & <@&962812802840539136>'s", inline=False)
-        embed.add_field(name="Ping", value="`/ping` \n Shows <@963255439963869244>'s latency", inline=False)
-        embed.add_field(name="Playlist Link", value="`/playlist` \n Sends the link to the robotics Spotify playlist", inline=False)
-        embed.add_field(name="Images Link", value="`/images` \n Sends an embed with the link to the Google Drive folder with all the photos", inline=False)
+        embed.add_field(
+            name="Annoy Someone",
+            value="`/annoy` \n Pings a given user a specified number of times. Only works for <@&918640986618486794>'s <@&918638863516328056>'s & <@&962812802840539136>'s",
+            inline=False,
+        )
+        embed.add_field(
+            name="Ping",
+            value="`/ping` \n Shows <@963255439963869244>'s latency",
+            inline=False,
+        )
+        embed.add_field(
+            name="Playlist Link",
+            value="`/playlist` \n Sends the link to the robotics Spotify playlist",
+            inline=False,
+        )
+        embed.add_field(
+            name="Images Link",
+            value="`/images` \n Sends an embed with the link to the Google Drive folder with all the photos",
+            inline=False,
+        )
         await ctx.send(embeds=embed)
 
-def setup(bot):
-    help(bot)
+def setup(bot: interactions.Client) -> None:
+    Help(bot)
