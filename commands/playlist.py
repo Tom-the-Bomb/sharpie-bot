8af537b0ab832c866eb9965b3a4936172cf7f97a
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-09-20 09:30:52 -0400

    refactor playlist.py

--- commands/playlist.py
+++ commands/playlist.py
@@ -1,14 +1,18 @@
 import interactions
 
-guild_ids = [918591198799749240]
+GUILD_IDS = [918591198799749240]
 
-class playlist(interactions.Extension):
-    def __init__(self, bot):
-        self.bot: interactions.Client = bot
+class Playlist(interactions.Extension):
+    def __init__(self, bot: interactions.Client) -> None:
+        self.bot = bot
 
     @interactions.extension_command(name="playlist", description="Sends a link to the robotics playlist on Spotify")
-    async def _playlist(self, ctx):
-        await ctx.send("**Quick Disclaimer:** This playlist is a compilation of everyone's different music tastes which means it'll mess with your head in ways you couldn't imagine so listening to it is done at your own risk.\n\nhttps://open.spotify.com/playlist/3S9LxhvckvM5CNMF7nhxmc?si=d37db241881d468d")
+    async def playlist(self, ctx: interactions.CommandContext) -> None:
+        await ctx.send(
+            "**Quick Disclaimer:** This playlist is a compilation of everyone's different music tastes"
+            "which means it'll mess with your head in ways you couldn't imagine so listening to it is done at your own risk.\n\n"
+            "https://open.spotify.com/playlist/3S9LxhvckvM5CNMF7nhxmc?si=d37db241881d468d"
+        )
 
-def setup(bot):
-    playlist(bot)
+def setup(bot: interactions.Client) -> None:
+    Playlist(bot)
