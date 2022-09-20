6ce12a691bbaef5d20a575bb49f9f132cb6af30b
JacobK <techsupport@kreindler.ca>
2022-09-19 21:52:33 -0400

    fix: removed schedule command

--- commands/help.py
+++ commands/help.py
@@ -15,7 +15,6 @@ class help(interactions.Extension):
         embed.add_field(name="Annoy Someone", value="`/annoy` \n Pings a given user a specified number of times. Only works for <@&918640986618486794>'s <@&918638863516328056>'s & <@&962812802840539136>'s", inline=False)
         embed.add_field(name="Ping", value="`/ping` \n Shows <@963255439963869244>'s latency", inline=False)
         embed.add_field(name="Playlist Link", value="`/playlist` \n Sends the link to the robotics Spotify playlist", inline=False)
-        embed.add_field(name="Schedule", value="`/schedule` \n Sends an embed with the schedule for the robotics meetings", inline=False)
         embed.add_field(name="Images Link", value="`/images` \n Sends an embed with the link to the Google Drive folder with all the photos", inline=False)
         await ctx.send(embeds=embed)
 
