d6c825355ebc7f598f61842181b734006b0ed96a
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-09-20 09:29:39 -0400

    refactor ping.py

--- commands/ping.py
+++ commands/ping.py
@@ -1,14 +1,14 @@
 import interactions
 
-guild_ids = [918591198799749240]
+GUILD_IDS = [918591198799749240]
 
-class ping(interactions.Extension):
-    def __init__(self, bot):
-        self.bot: interactions.Client = bot
+class Ping(interactions.Extension):
+    def __init__(self, bot: interactions.Client) -> None:
+        self.bot = bot
 
     @interactions.extension_command(name="ping", description="Shows bot latency")
-    async def _ping(self, ctx):
+    async def ping(self, ctx: interactions.CommandContext) -> None:
         await ctx.send("Hello! I'm Sharpie!")
 
-def setup(bot):
-    ping(bot)
+def setup(bot: interactions.Client) -> None:
+    Ping(bot)
