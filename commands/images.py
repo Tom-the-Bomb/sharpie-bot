f49a319d8b34be7e133df4a96ba611d06583899f
Tom-the-Bomb <77308095+Tom-the-Bomb@users.noreply.github.com>
2022-09-20 09:23:17 -0400

    refactor images.py

--- commands/images.py
+++ commands/images.py
@@ -1,14 +1,18 @@
 import interactions
 
-guild_ids = [918591198799749240]
-class images(interactions.Extension):
-    def __init__(self, bot):
-        self.bot: interactions.Client = bot
+GUILD_IDS = [918591198799749240]
 
-    @interactions.extension_command(name="images", description="Link for the google drive with all the images", options=[])
-    async def image(self, ctx):
-        embed = interactions.Embed(title="📸 Robotics Media Folder", description="All of the pictures and videos we've taken have been uploaded to the [Google Drive folder](https://drive.google.com/drive/folders/1Huch-lIgYjOjULL7cKV6anj-u5IHbLX1?usp=sharing) ")
+class Images(interactions.Extension):
+    def __init__(self, bot: interactions.Client) -> None:
+        self.bot = bot
+
+    @interactions.extension_command(name="images", description="Link for the google drive with all the images")
+    async def image(self, ctx: interactions.CommandContext) -> None:
+        embed = interactions.Embed(
+            title="📸 Robotics Media Folder",
+            description="All of the pictures and videos we've taken have been uploaded to the [Google Drive folder](https://drive.google.com/drive/folders/1Huch-lIgYjOjULL7cKV6anj-u5IHbLX1?usp=sharing) "
+        )
         await ctx.send(embeds=embed)
 
-def setup(bot):
-    images(bot)
+def setup(bot: interactions.Client) -> None:
+    Images(bot)
